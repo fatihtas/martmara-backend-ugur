6cfe7e318fe5bd83169e37f975fa6eb96c2b6e82
borakrc <borakrc@gmail.com>
2015-07-25 14:21:20 +0300

    /services/searchservice.js gets keys from here

--- config/appkeys.js
+++ config/appkeys.js
@@ -11,6 +11,11 @@ var accountSID  = 'ACa049468c4507ced1d2aea094773710d6';
 var authToken   = '6d68e56aa57aafec4cbd87bac348cf46';
 var twilioPhone = "+12017318358";
 
+//  /services/searchservice.js
+var FOURSQUARE_CLIENT_ID     = 'SSKIRMC24PBSSQ25VMSD30OSHLMLUNPJ2W0EMQJE1N2EHU5A';
+var FOURSQUARE_CLIENT_SECRET = 'RSZO23NOHYMHZXGA5QYW22EBFIDCCS04WCMYVAOEMI3N5VWR';
+
+
 exports.FACEBOOK_CLIENT_ID = FACEBOOK_CLIENT_ID;
 exports.FACEBOOK_CLIENT_SECRET = FACEBOOK_CLIENT_SECRET;
 
@@ -23,5 +28,9 @@ exports.JWT_SECRET_TOKEN = JWT_SECRET_TOKEN;
 exports.accountSID = accountSID;
 exports.authToken = authToken;
 exports.twilioPhone = twilioPhone;
+
+//  /services/searchservice.js
+exports.FOURSQUARE_CLIENT_ID = FOURSQUARE_CLIENT_ID;
+exports.FOURSQUARE_CLIENT_SECRET = FOURSQUARE_CLIENT_SECRET;
 
 
